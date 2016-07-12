bce97b0e8a2b785cd53644f42e6cacb3bc2c35ad
Adina Bianchi <curiousloop@Adinas-MacBook-Air.local>
2016-07-12 15:45:32 -0400

    all tests pass

--- locatable_spec.rb
+++ locatable_spec.rb
@@ -16,57 +16,65 @@ module Locatable
 
   module ClassMethods
 
-    def closest_to lat, long
-      @points= self.all
+    def closest_to lat, long, ops=nil
+      @points = self.all 
+
       miles = {}
+
       @points.each do |s|
         distance = Haversine.distance(lat, long, s.latitude, s.longitude).to_m
         miles[s] = distance 
       end
-      sorted = miles.sort_by { |k,v| v}
-      return sorted.first[0]
+      @sorted = miles.sort_by { |k,v| v}
+
+      if ops 
+        c = ops[:count]
+        filtered =  @sorted.first(2)
+        return filtered.map { |s| s[0] }
+      else
+        return closest = @sorted.first[0]
+      end
+    end
+  end
+end
+
+  class Station
+    attr_reader :latitude, :longitude
+
+    include Locatable
+
+    def initialize lat, long
+      @latitude, @longitude = lat, long
     end
 
-  end
-end
-
-class Station
-  attr_reader :latitude, :longitude
-
-  include Locatable
-
-  def initialize lat, long
-    @latitude, @longitude = lat, long
+    def self.all
+      [
+        Station.new(12, -36),
+        Station.new(10, -33),
+        Station.new(77,  45)
+      ]
+    end
   end
 
-  def self.all
-    [
-      Station.new(12, -36),
-      Station.new(10, -33),
-      Station.new(77,  45)
-    ]
+  describe Locatable do
+    it "can find distances" do
+      s = Station.new 10, -33
+
+      expect(s.distance_to 10, -33).to eq 0.0
+      expect(s.distance_to 10, -34).to be < 200000.0
+    end
+
+    it "can find closest stations" do
+      s = Station.closest_to 10, -34
+
+      expect(s.latitude ).to eq  10
+      expect(s.longitude).to eq -33
+    end
+
+    it "can find list of closest" do
+      s = Station.closest_to 10, -34, count: 2
+
+      expect(s.count).to eq 2
+      expect(s.last.latitude).to eq 12
+    end
   end
-end
-
-describe Locatable do
-  it "can find distances" do
-    s = Station.new 10, -33
-
-    expect(s.distance_to 10, -33).to eq 0.0
-    expect(s.distance_to 10, -34).to be < 200000.0
-  end
-
-  it "can find closest stations" do
-    s = Station.closest_to 10, -34
-
-    expect(s.latitude ).to eq  10
-    expect(s.longitude).to eq -33
-  end
-
-  skip "can find list of closest" do
-    s = Station.closest_to 10, -34, count: 2
-
-    expect(s.count).to eq 2
-    expect(s.last.latitude).to eq -36
-  end
-end
